5eae845188dfbcc4e3d9afab0e743e6ed9387e53
Txythread <txyzthread@icloud.com>
2026-02-20 21:33:41 +0100

    Added Death

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,8 +13,7 @@ public class Player : MonoBehaviour
     ///  Gravity in m/s.
     /// </summary>
     [SerializeField] private float Gravity;
-    private RaycastHit2D _rayBuffer;
-    private Vector3 _bottomPos;
+    
     [SerializeField] private bool grounded;
     [SerializeField] private GameObject center;
     [SerializeField] private float velocityY = 0;
@@ -26,9 +25,14 @@ public class Player : MonoBehaviour
     /// </summary>
     [SerializeField] private float rotationSpeedOnGround;
     [FormerlySerializedAs("targetRotation")] [SerializeField] private float targetRotationDistance;
+    
     private const float PlayerHeight = 1f;
     private bool _lastFrameGrounded = false;
-    private byte jumpingBlockedFrames = 0;
+    private byte _jumpingBlockedFrames;
+    
+    private RaycastHit2D _rayBuffer;
+    private Vector3 _bottomPos;
+    private Vector3 _forwardPos;
     
 
     private ControlMode mode = ControlMode.Normal;
@@ -42,11 +46,14 @@ public class Player : MonoBehaviour
         
         gameActionMap.FindAction("Jump").Enable();
         gameActionMap.FindAction("Jump").performed += _ => Jump();
+        
+        
+        _bottomPos = transform.position + Vector3.down * (PlayerHeight / 2 + 0.01f);
+        _forwardPos = transform.position + Vector3.right * (PlayerHeight / 2 - 0.1f);
     }
 
     public void Jump()
     {
-        Debug.Log("Jumping - v0.0");
         // The minimal height for the player to count as ungrounded.
         SetPositionOnGround();
         if (grounded)
@@ -56,7 +63,7 @@ public class Player : MonoBehaviour
             velocityY = jumpVelocity;
         }
 
-        jumpingBlockedFrames = 3;
+        _jumpingBlockedFrames = 3;
     }
 
     public void SetPositionOnGround(int depth = 0)
@@ -64,7 +71,6 @@ public class Player : MonoBehaviour
         const int maxDepth = 5;
         
         // The minimal height for the player to count as ungrounded.‚
-        _bottomPos = transform.position + Vector3.down * (PlayerHeight / 2 + 0.01f);
         _rayBuffer = Physics2D.Raycast(center.transform.position, Vector2.down, 1f);
         float minHeightUngrounded = _rayBuffer.distance - 0.5005f;
 
@@ -83,10 +89,68 @@ public class Player : MonoBehaviour
     }
 
 
+    private void CheckDead()
+    {
+        // Check if blocked in front
+        bool hit = false;
+        
+        const float rayLength = 0.01f;
+        
+        // Find the bottom
+
+        if (Debug.isDebugBuild)
+        {
+            Debug.DrawLine(_bottomPos, _bottomPos + (Vector3.down * rayLength), Color.brown);
+        }
+        
+        _rayBuffer = Physics2D.Raycast(_forwardPos, Vector2.down, 0.01f, LayerMask.GetMask("Wall"));
+        hit |= _rayBuffer.collider != null;
+        _rayBuffer = Physics2D.Raycast(_forwardPos - Vector3.up * 0.5f, Vector2.down, 0.01f, LayerMask.GetMask("Wall"));
+        hit |= _rayBuffer.collider != null;
+        _rayBuffer = Physics2D.Raycast(_forwardPos - Vector3.down * 0.5f, Vector2.down, 0.01f, LayerMask.GetMask("Wall"));
+        hit |= _rayBuffer.collider != null;
+
+        if (hit)
+        {
+            Dead();
+        }
+    }
+
+    private bool CheckGrounded()
+    {
+        const float rayLength = 0.01f;
+        bool wallHitBottom = false;
+        
+        // Find the bottom
+
+        if (Debug.isDebugBuild)
+        {
+            Debug.DrawLine(_bottomPos, _bottomPos + (Vector3.down * rayLength), Color.green);
+        }
+        
+        _bottomPos = transform.position + Vector3.down * (PlayerHeight / 2 + 0.01f);
+        
+        _rayBuffer = Physics2D.Raycast(_bottomPos, Vector2.down, 0.01f, LayerMask.GetMask("Wall"));
+        wallHitBottom |= _rayBuffer.collider != null;
+        _rayBuffer = Physics2D.Raycast(_bottomPos - Vector3.left * 0.5f, Vector2.down, 0.01f, LayerMask.GetMask("Wall"));
+        wallHitBottom |= _rayBuffer.collider != null;
+        _rayBuffer = Physics2D.Raycast(_bottomPos - Vector3.right * 0.5f, Vector2.down, 0.01f, LayerMask.GetMask("Wall"));
+        wallHitBottom |= _rayBuffer.collider != null;
+
+        grounded = wallHitBottom;
+
+        return wallHitBottom;
+    }
+    
+    private void Dead()
+    {
+        Destroy(gameObject);
+    }
+
+
     // Update is called once per frame
     void LateUpdate()
     {
-        const float rayLength = 0.01f;
         float currentRotation = transform.rotation.eulerAngles.z;
 
 
@@ -101,27 +165,8 @@ public class Player : MonoBehaviour
         var posChangeY = velocityY;
 
 
-        // Find the bottom
-        _bottomPos = transform.position + Vector3.down * (PlayerHeight / 2 + 0.01f);
-
-        if (Debug.isDebugBuild)
+        if (CheckGrounded())
         {
-            Debug.DrawLine(_bottomPos, _bottomPos + (Vector3.down * rayLength), Color.green);
-        }
-
-        bool wallHitBottom = false;
-
-        _rayBuffer = Physics2D.Raycast(_bottomPos, Vector2.down, 0.01f, LayerMask.GetMask("Wall"));
-        wallHitBottom |= _rayBuffer.collider != null;
-        _rayBuffer = Physics2D.Raycast(_bottomPos - Vector3.left * 0.5f, Vector2.down, 0.01f, LayerMask.GetMask("Wall"));
-        wallHitBottom |= _rayBuffer.collider != null;
-        _rayBuffer = Physics2D.Raycast(_bottomPos - Vector3.right * 0.5f, Vector2.down, 0.01f, LayerMask.GetMask("Wall"));
-        wallHitBottom |= _rayBuffer.collider != null;
-
-        if (wallHitBottom)
-        {
-            grounded = true;
-            
             targetRotationDistance = -((currentRotation % 180) - 90);
             
             if (Mathf.Abs(targetRotationDistance) < 5)
@@ -130,13 +175,9 @@ public class Player : MonoBehaviour
             }
             
         }
-        else
-        {
-            grounded = false;
-        }
         
 
-        if (grounded && jumpingBlockedFrames == 0) 
+        if (grounded && _jumpingBlockedFrames == 0) 
         {
             posChangeY = 0;
             velocityY = 0;
@@ -179,10 +220,12 @@ public class Player : MonoBehaviour
         
         gameObject.transform.position += Vector3.up * posChangeY;
 
-        if (jumpingBlockedFrames > 0)
+        if (_jumpingBlockedFrames > 0)
         {
-            jumpingBlockedFrames--;
+            _jumpingBlockedFrames--;
         }
+        
+        CheckDead();
     }
 }
 
