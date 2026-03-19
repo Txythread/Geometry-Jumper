cffabba4bbe20b02792d9235262d2be6f3e38c31
Txythread <txyzthread@icloud.com>
2026-03-19 21:17:45 +0100

    Added Particles to Orbs

--- Assets/Scripts/Orb.cs
+++ Assets/Scripts/Orb.cs
@@ -11,20 +11,30 @@ public class Orb : LevelObject
     /// </summary>
     private const float InversionTime = 0.25f;
 
-    private const float GrowthMultiplier = 1f;
+    private const float GrowthMultiplier = 1.5f;
     
     [SerializeField] private GameObject mainOrb;
     [SerializeField] private GameObject surroundingCircle;
+    [SerializeField] private ParticleSystem particles;
 
     [SerializeField] private Color color;
 
     private float _currentStageTimeCounter;
-    private bool _isGrowing;
+    private bool _isGrowing = true;
+    private float _originalSurrounderSize;
 
     protected override void Awake()
     {
         mainOrb.GetComponent<SpriteRenderer>().color = color.WithAlpha(1f);
         surroundingCircle.GetComponent<SpriteRenderer>().color = color.WithAlpha(0.5f);
+
+        var main = particles.main;
+        main.startColor = color.WithAlpha(1f);
+
+        var colorOverLifetime = particles.colorOverLifetime;
+        colorOverLifetime.color = new ParticleSystem.MinMaxGradient(color, Color.white.WithAlpha(0.2f));
+        
+        _originalSurrounderSize = surroundingCircle.transform.localScale.x;
     }
 
     protected override void EndUpdate()
@@ -34,6 +44,11 @@ public class Orb : LevelObject
         {
             _currentStageTimeCounter = 0;
             _isGrowing = !_isGrowing;
+
+            if (_isGrowing)
+            {
+                surroundingCircle.gameObject.transform.localScale = new Vector3(_originalSurrounderSize, _originalSurrounderSize, 1);
+            }
         }
 
         var growth = GrowthMultiplier * Time.deltaTime;
